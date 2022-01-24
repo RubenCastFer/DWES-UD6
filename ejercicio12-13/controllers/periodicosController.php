958b82fdd51ee9d5d5978f8dff1deeca8cf7c96e
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-01-24 11:48:29 +0100

    13

--- ejercicio12-13/controllers/periodicosController.php
+++ ejercicio12-13/controllers/periodicosController.php
@@ -7,7 +7,7 @@
         include './views/periodicosView.php';
 
     }
-//http://localhost/DWES/EJERCICIO_DE_CLASE/DWES-UD6/ejercicio12/index.php?controller=periodicos&action=mostraPeriodico&num=3
+//http://localhost/DWES/EJERCICIO_DE_CLASE/DWES-UD6/ejercicio12-13/index.php?controller=periodicos&action=mostraPeriodico&num=3
     function mostraPeriodico(){
         require './models/periodicosModel.php';
         $periodicos=getPeriodicoConcreto($_GET['num']);
